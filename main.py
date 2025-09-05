fb3ca4f38e258e39fa5ae9f97da4e0891dd96616
williamcdavies <152814048+williamcdavies@users.noreply.github.com>
2025-09-05 15:45:26 -0700

    Enable table crawling

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
 import csv
 
-with open('in.csv') as csvfile:
-    reader = csv.reader(csvfile, delimiter=',')
-    for row in reader:
-        print(', '.join(row))
+from playwright.sync_api import sync_playwright
 
-# from playwright.sync_api import sync_playwright
+with open('input.csv', newline='') as f:
+    rdr = csv.DictReader(f)
+    lst = list(rdr)
 
-# with sync_playwright() as p:
-#     browser = p.firefox.launch(headless=True)
-#     page = browser.new_page()
-#     page.goto("https://www.unr.edu/admissions/records/academic-calendar/finals-schedule")
-#     print(page.title())
-#     browser.close()
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=True)
+    page = browser.new_page()
+    page.goto("https://www.unr.edu/admissions/records/academic-calendar/finals-schedule")
+    
+    tr_locator = page.locator("tr")
+
+    for n in range(tr_locator.count()):
+        nth_tr_locator = tr_locator.nth(n)
+        td_locator = nth_tr_locator.locator("td")
+        
+        if td_locator.count() != 3:
+            continue
+        
+        for item in lst:
+            if(item['course_start_time'] in td_locator.nth(0).inner_text().strip() and item['course_schedule'] in td_locator.nth(1).inner_text().strip()):
+                print(td_locator.nth(2).inner_text().strip())
+    
+    browser.close()
